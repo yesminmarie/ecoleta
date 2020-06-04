0030b32d9b662ebaa52d63fd4eb446304e52ac82
yesmin <ymslahoud@gmail.com>
2020-06-03 21:49:57 -0300

    back-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import path from 'path'
 import routes from './routes';
 
 const app = express();
 
+app.use(cors()); //define quais urls podem acessar a API
 app.use(express.json());
-
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
